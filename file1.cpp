ca2b23b0e6db086734d398eb97d8f9f768bc09e1
atharvp4098 <atharvp4098@gmail.com>
2026-07-21 09:46:54 +0000

    the compilation of 2 files

---
+++ file1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+namespace sycs4
+{
+    int add()
+    {
+        int a,b,sum;
+      std:: cout<<"Enter the number";
+      std::cin>> a>>b;
+      std::cout<<"Sum of two numbers is"<<(a+b);
+        return 0;
+    }
+}
